227792eea6b909eb7d5e5010e916c66875ea9334
Christopher Okokon Ntuk <engchris95@gmail.com>
2019-09-17 11:49:36 +0100

    Styling change and Welcome Page update

--- dashboard.php
+++ dashboard.php
@@ -7,33 +7,69 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Techy Blinders - dashboard</title>
-   <!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
-<!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
-   
-	<!--Bootsrap 4 CDN-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    
-    <!--Fontawesome CDN-->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-
-	<!--Custom style-->
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+<style>
+  body, html {
+    height: 100%;
+    margin: 0;
+  }
+  #bg {
+    background-image: url("http://getwallpapers.com/wallpaper/full/a/5/d/544750.jpg");
+    height: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    text-align: center;
+    padding: 20px;
+  }
+  #ag {
+    background-color: rgba(20, 17, 20, 0.322);
+    width: 300px;
+    height: 300px;
+    padding: 100px;
+    align-self: center;
+    margin: 0 auto;
+    align-content: center;
+  }
+  #one {
+    font-family: "Bradley Hand ITC";
+    font-style: italic;
+  }
+  #two {
+    font-family: "Lucida Console";
+    font-size: larger;
+  }
+  .color-brown {
+    color: white; 
+  }
+  .button { 
+    background-color:yellow;
+    border: none;
+    color: black;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+  }
+</style>
 </head>
 <body>
-<div class="container">
-	<div class="d-flex justify-content-center h-100">
-		<div class="card" style="color: #fff">
-      <h3>Welcome, <?php echo ucfirst($_SESSION['username'])?></h3>
-      <p>This is Techy-Blinders welcome page. Feel free to walk around. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima maiores saepe nisi suscipit dolore est praesentium quam eligendi beatae impedit natus blanditiis voluptatibus quas iusto aut, ea cupiditate pariatur corrupti?</p>
-			<a href="script/logout.php">
-				<button class="btn btn-danger">Logout</button>
-			</a>
-		</div>
-	</div>
-</div>
-<script src="js/jquery.min.js"></script>
-<script src="js/custom.js"></script>
+   <div id="bg" class="color-brown" >
+       <div id="ag">
+           <div id="one">
+               <h3>LOGIN SUCCESSFUL</h3>
+           </div>
+           <h2>Welcome, <?php echo ucfirst($_SESSION['username'])?></h2>
+           <div id="two">
+              <h2>WELCOME TO TECHY BLINDER!</h2>
+           </div>
+           <div>
+            <a href="script/logout.php" class="button">
+              Logout
+            </a>
+       </div>
+       </div>
+   </div>
 </body>
 </html>
